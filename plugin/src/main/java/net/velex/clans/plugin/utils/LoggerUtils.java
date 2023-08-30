f87e73bc4d51f6a3ea187aa0c82c4b7f7d19badf
InitSync <benjaalejandro206@gmail.com>
2023-08-30 12:42:16 -0400

    feat(utils): implement multiple utils for the plugin internal usage

---
+++ plugin/src/main/java/net/velex/clans/plugin/utils/LoggerUtils.java
@@ -0,0 +1,29 @@
+package net.velex.clans.plugin.utils;
+
+import net.velex.clans.plugin.ClansPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LoggerUtils {
+  private static final Logger LOGGER = ClansPlugin.getPlugin(ClansPlugin.class).getLogger();
+  
+  public static void info(final String @NotNull... content) {
+    for (final var message : content) {
+      LOGGER.log(Level.INFO, message);
+    }
+  }
+  
+  public static void warn(final String @NotNull... content) {
+    for (final var message : content) {
+      LOGGER.log(Level.WARNING, message);
+    }
+  }
+  
+  public static void error(final String @NotNull... content) {
+    for (final var message : content) {
+      LOGGER.log(Level.SEVERE, message);
+    }
+  }
+}
---
+++ plugin/src/main/java/net/velex/clans/plugin/utils/location/LocationUtils.java
@@ -0,0 +1,61 @@
+package net.velex.clans.plugin.utils.location;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.jetbrains.annotations.NotNull;
+
+public class LocationUtils {
+	private static final StringBuilder BUILDER = new StringBuilder();
+	
+	public static @NotNull Location from(final @NotNull String str) {
+		final var split = str.split(";");
+    // Checks if the array length is equal to six.
+		if (split.length == 6) {
+			return new Location(
+				Bukkit.getWorld(split[0]),
+				Double.parseDouble(split[1]),
+				Double.parseDouble(split[2]),
+				Double.parseDouble(split[3]),
+				Float.parseFloat(split[4]),
+				Float.parseFloat(split[5])
+			);
+		}
+		return new Location(
+			Bukkit.getWorld(split[0]),
+			Double.parseDouble(split[1]),
+			Double.parseDouble(split[2]),
+			Double.parseDouble(split[3])
+		);
+	}
+
+	public static @NotNull String to(final @NotNull Location loc, final boolean yawPitch) {
+    final var world = loc.getWorld();
+    // Checks if the location world is null.
+    if (world == null) {
+      return "";
+    }
+    // Checks if the 'yawPitch' value is true.
+		if (yawPitch) {
+			return BUILDER.append(world.getName())
+				.append(";")
+				.append(loc.getX())
+				.append(";")
+				.append(loc.getY())
+				.append(";")
+				.append(loc.getZ())
+				.append(";")
+				.append(loc.getYaw())
+				.append(";")
+				.append(loc.getPitch())
+				.toString();
+		}
+		return BUILDER.append(world.getName())
+			.append(";")
+			.append(loc.getX())
+			.append(";")
+			.append(loc.getY())
+			.append(";")
+			.append(loc.getZ())
+			.toString();
+	}
+}
