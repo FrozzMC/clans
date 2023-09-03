6a8d3a2c7c5a2b427460dbb64b6ea9b1b57a9e97
InitSync <benjaalejandro206@gmail.com>
2023-09-03 18:01:33 -0300

    chore(ClansPlugin): change fields type for the new managers and apply API interface changes

--- plugin/src/main/java/net/velex/clans/plugin/ClansPlugin.java
+++ plugin/src/main/java/net/velex/clans/plugin/ClansPlugin.java
@@ -2,47 +2,41 @@ package net.velex.clans.plugin;
 
 import net.velex.clans.api.Clans;
 import net.velex.clans.api.Provider;
+import net.velex.clans.api.clan.ClanChatManager;
+import net.velex.clans.api.clan.ClanManager;
+import net.velex.clans.api.config.ConfigManager;
 import net.velex.clans.api.enums.Result;
+import net.velex.clans.api.hook.HookManager;
 import net.velex.clans.api.model.*;
-import net.velex.clans.plugin.impl.ClanChatHandlerImpl;
-import net.velex.clans.plugin.impl.SimpleAntiFarmManagerImpl;
-import net.velex.clans.plugin.impl.SimpleConfManagerModelImpl;
-import net.velex.clans.plugin.impl.SimpleHookManagerImpl;
 import net.velex.clans.plugin.utils.LoggerUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 public final class ClansPlugin extends JavaPlugin implements Clans {
-  private ConfManagerModel confManagerModel;
-  private HookManagerModel hookManagerModel;
-  private ClanManagerModel clanManagerModel;
-  private ClanTopManagerModel clanTopManagerModel;
-  private ClanChatHandlerModel clanChatHandlerModel;
+  private ConfigManager configManager;
+  private HookManager hookManager;
+  private ClanManager clanManager;
+  private ClanChatManager clanChatManager;
   private AntiFarmManagerModel antiFarmManagerModel;
 
   @Override
-  public @NotNull ConfManagerModel confManagerModel() {
-    return confManagerModel;
+  public @NotNull ConfigManager configManager() {
+    return configManager;
   }
   
   @Override
-  public @NotNull ClanManagerModel clanManagerModel() {
-    return clanManagerModel;
+  public @NotNull ClanManager clanManager() {
+    return clanManager;
   }
   
   @Override
-  public @NotNull ClanTopManagerModel clanTopManagerModel() {
-    return clanTopManagerModel;
+  public @NotNull ClanChatManager clanChatManager() {
+    return clanChatManager;
   }
   
   @Override
-  public @NotNull ClanChatHandlerModel clanChatHandlerModel() {
-    return clanChatHandlerModel;
-  }
-  
-  @Override
-  public @NotNull HookManagerModel hookManagerModel() {
-    return hookManagerModel;
+  public @NotNull HookManager hookManager() {
+    return hookManager;
   }
 
   @Override
@@ -55,22 +49,22 @@ public final class ClansPlugin extends JavaPlugin implements Clans {
     Provider.load(this);
     
     LoggerUtils.info("Loading configuration components for the plugin...");
-    confManagerModel = new SimpleConfManagerModelImpl(getDataFolder().toPath());
+    configManager = new ConfigManager(getDataFolder().toPath());
     // Checks if some configuration model were not loaded correctly.
-    if (confManagerModel.load() == Result.NO_CONFIG_LOAD) {
+    if (configManager.load() == Result.NO_CONFIG_LOAD) {
       LoggerUtils.error("Some configuration files could not be loaded correctly.");
       setEnabled(false);
       return;
     }
-    hookManagerModel = new SimpleHookManagerImpl(getServer().getPluginManager());
-    clanChatHandlerModel = new ClanChatHandlerImpl(clanManagerModel);
-    antiFarmManagerModel = new SimpleAntiFarmManagerImpl();
+    clanManager = new ClanManager();
+    hookManager = new HookManager(getServer().getPluginManager());
+    clanChatManager = new ClanChatManager();
   }
   
   @Override
   public void onEnable() {
     // Checks if the 'hook()' method execution result is a 'NO_HOOKED' enum type.
-    if (hookManagerModel.hook() == Result.NO_HOOKED) {
+    if (hookManager.hook() == Result.NO_HOOKED) {
       LoggerUtils.error("Some dependency could not be hooked by the plugin, check if they are installed and update.");
     }
   }
@@ -83,13 +77,9 @@ public final class ClansPlugin extends JavaPlugin implements Clans {
       "Disabling plugin internal components...",
       "Clearing cache memory used by the plugin..."
     );
-    // Checks if the ClansManagerModel reference has not been deleted.
-    if (clanManagerModel != null) {
-      clanManagerModel.clear();
-    }
-    // Checks if the ClanTopManagerModel reference has not been deleted.
-    if (clanTopManagerModel != null) {
-      clanTopManagerModel.clear();
+    // Checks if the ClanManagerModel reference has not been deleted.
+    if (clanManager != null) {
+      clanManager.clear();
     }
   }
 }
