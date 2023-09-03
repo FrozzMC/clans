b4d9fbcde1f54cec9c1ed74a3527ebd170dd011d
InitSync <benjaalejandro206@gmail.com>
2023-09-03 17:50:06 -0300

    refactor: remove old internal models

--- api/src/main/java/net/velex/clans/api/model/HookManagerModel.java
+++
@@ -1,8 +0,0 @@
-package net.velex.clans.api.model;
-
-import net.velex.clans.api.enums.Result;
-import org.jetbrains.annotations.NotNull;
-
-public interface HookManagerModel {
-  @NotNull Result hook();
-}
