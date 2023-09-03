73fe5744025967b66231a05a365b1021cb643ecc
InitSync <benjaalejandro206@gmail.com>
2023-09-03 17:51:43 -0300

    feat(Result): add more constants to the enum

--- api/src/main/java/net/velex/clans/api/enums/Result.java
+++ api/src/main/java/net/velex/clans/api/enums/Result.java
@@ -16,5 +16,37 @@ public enum Result {
   /**
    * Indicates that there wasn't a correct handler for the operation.
    */
-  NO_HANDLE
+  NO_HANDLE,
+  /**
+   * Indicates that the clan received a leader change.
+   */
+  SHOULD_LEADER_CHANGE,
+  /**
+   * Indicates that the leader specified for the change is the same that the old leader.
+   */
+  SAME_CLAN_LEADER,
+  /**
+   * Indicates that the clan was deleted correctly.
+   */
+  SHOULD_CLAN_DELETE,
+  /**
+   * Indicates that the clan cannot be deleted fully.
+   */
+  NO_CLAN_DELETE,
+  /**
+   * Indicates that the clan specified is already created.
+   */
+  ALREADY_CLAN_CREATED,
+  /**
+   * Indicates that the member cannot be removed from clan.
+   */
+  NO_MEMBER_REMOVE,
+  /**
+   * Indicates that the member were removed from clan.
+   */
+  SHOULD_MEMBER_REMOVE,
+  /**
+   * Indicates that the member is already removed from clan.
+   */
+  ALREADY_MEMBER_REMOVE
 }
