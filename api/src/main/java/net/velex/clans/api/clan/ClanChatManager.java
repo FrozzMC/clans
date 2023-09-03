9f913a198657aa2d121ea3cf7c11483432508890
InitSync <benjaalejandro206@gmail.com>
2023-09-03 18:02:15 -0300

    feat(ClanChatManager): implement base for the clan chat manager

---
+++ api/src/main/java/net/velex/clans/api/clan/ClanChatManager.java
@@ -0,0 +1,22 @@
+package net.velex.clans.api.clan;
+
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class ClanChatManager {
+  public void handle(final @NotNull AsyncPlayerChatEvent event) {
+  
+  }
+  
+  public void performClanMessage(final @NotNull String id, final @NotNull String message) {
+  
+  }
+  
+  public void performAllyMessage(final @NotNull String id, final @NotNull String message) {
+  
+  }
+  
+  public void performTruceMessage(final @NotNull String id, final @NotNull String message) {
+  
+  }
+}
