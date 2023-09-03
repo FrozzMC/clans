b3be7c10868374987d4b215eba68110aad31ac76
InitSync <benjaalejandro206@gmail.com>
2023-09-03 17:57:02 -0300

    chore: use ClanModel for clan data in events

--- api/src/main/java/net/velex/clans/api/event/ClanKickEvent.java
+++ api/src/main/java/net/velex/clans/api/event/ClanKickEvent.java
@@ -1,7 +1,7 @@
 package net.velex.clans.api.event;
 
 import com.google.common.base.Preconditions;
-import net.velex.clans.api.model.internal.ClanDataInternalModel;
+import net.velex.clans.api.clan.ClanModel;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
@@ -9,8 +9,12 @@ public class ClanKickEvent extends ClanEvent {
   private final Player kickedPlayer;
   private final String executorPlayerName;
   
-  public ClanKickEvent(final @NotNull ClanDataInternalModel clanDataInternalModel, final @NotNull Player kickedPlayer, final @NotNull String executorPlayerName) {
-    super(clanDataInternalModel);
+  public ClanKickEvent(
+    final @NotNull ClanModel clanModel,
+    final @NotNull Player kickedPlayer,
+    final @NotNull String executorPlayerName
+  ) {
+    super(clanModel);
     this.kickedPlayer = Preconditions.checkNotNull(kickedPlayer, "Kicked player reference cannot be null.");
     this.executorPlayerName = Preconditions.checkNotNull(executorPlayerName, "Executor player name cannot be null.");
   }
--- api/src/main/java/net/velex/clans/api/event/ClanLeaveEvent.java
+++ api/src/main/java/net/velex/clans/api/event/ClanLeaveEvent.java
@@ -1,15 +1,15 @@
 package net.velex.clans.api.event;
 
 import com.google.common.base.Preconditions;
-import net.velex.clans.api.model.internal.ClanDataInternalModel;
+import net.velex.clans.api.clan.ClanModel;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class ClanLeaveEvent extends ClanEvent {
   private final Player player;
   
-  public ClanLeaveEvent(final @NotNull ClanDataInternalModel clanDataInternalModel, final @NotNull Player player) {
-    super(clanDataInternalModel);
+  public ClanLeaveEvent(final @NotNull ClanModel clanModel, final @NotNull Player player) {
+    super(clanModel);
     this.player = Preconditions.checkNotNull(player, "Player reference cannot be null.");
   }
   
