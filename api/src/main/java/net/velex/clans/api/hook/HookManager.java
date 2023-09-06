12529587ef664f310d2f877ff763de5aeb390114
InitSync <benjaalejandro206@gmail.com>
2023-09-05 22:43:49 -0300

    chore(HookManager): include results enum into the own class, and add documentation for the hook() method

--- api/src/main/java/net/velex/clans/api/hook/HookManager.java
+++ api/src/main/java/net/velex/clans/api/hook/HookManager.java
@@ -1,7 +1,6 @@
 package net.velex.clans.api.hook;
 
 import com.google.common.base.Preconditions;
-import net.velex.clans.api.enums.Result;
 import net.velex.clans.api.hook.guard.WorldGuardHook;
 import org.bukkit.plugin.PluginManager;
 import org.jetbrains.annotations.NotNull;
@@ -15,6 +14,12 @@ public class HookManager {
     this.pluginManager = Preconditions.checkNotNull(pluginManager, "PluginManager reference cannot be null.");
   }
   
+  /**
+   * Tries to hook the dependencies (optional) for the plugin, and return a {@link HookManager.Result} enum type.
+   *
+   * @return If all the dependencies were hooked correctly, will return the {@link HookManager.Result#SUCCESS} type.
+   * <p>Else, will return the {@link HookManager.Result#NO_HOOKED} type.
+   */
   public @NotNull Result hook() {
     // Checks if PlaceholderAPI is available on the server.
     if ((pluginManager.getPlugin("PlaceholderAPI") == null) || !pluginManager.isPluginEnabled("PlaceholderAPI")) {
@@ -36,5 +41,16 @@ public class HookManager {
       throw new IllegalStateException("Could not be get the WorldGuardHook reference.");
     }
     return worldGuardHook;
+  }
+  
+  public enum Result {
+    /**
+     * Indicates that the operation were completed correctly.
+     */
+    SUCCESS,
+    /**
+     * Indicates that some dependencies could not be hooked correctly.
+     */
+    NO_HOOKED
   }
 }
