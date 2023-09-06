c9e7d7a0a98e1e5841dc067a5aeaf0bc39c50bed
InitSync <benjaalejandro206@gmail.com>
2023-09-05 22:41:36 -0300

    chore(ClansPlugin): initialize anti-farm manager, call to ClanManager#loadClansData() in onEnable() method, and move API inherited methods to bellow of class

--- plugin/src/main/java/net/velex/clans/plugin/ClansPlugin.java
+++ plugin/src/main/java/net/velex/clans/plugin/ClansPlugin.java
@@ -2,12 +2,11 @@ package net.velex.clans.plugin;
 
 import net.velex.clans.api.Clans;
 import net.velex.clans.api.Provider;
+import net.velex.clans.api.antifarm.AntiFarmManager;
 import net.velex.clans.api.clan.ClanChatManager;
 import net.velex.clans.api.clan.ClanManager;
 import net.velex.clans.api.config.ConfigManager;
-import net.velex.clans.api.enums.Result;
 import net.velex.clans.api.hook.HookManager;
-import net.velex.clans.api.model.*;
 import net.velex.clans.plugin.utils.LoggerUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -17,8 +16,49 @@ public final class ClansPlugin extends JavaPlugin implements Clans {
   private HookManager hookManager;
   private ClanManager clanManager;
   private ClanChatManager clanChatManager;
-  private AntiFarmManagerModel antiFarmManagerModel;
+  private AntiFarmManager antiFarmManager;
 
+  @Override
+  public void onLoad() {
+    Provider.load(this);
+    
+    LoggerUtils.info("Loading configuration components for the plugin...");
+    configManager = new ConfigManager(getDataFolder().toPath());
+    // Checks if some configuration model were not loaded correctly.
+    if (configManager.load() == ConfigManager.Result.NO_CONFIG_LOAD) {
+      LoggerUtils.error("Some configuration files could not be loaded correctly.");
+      setEnabled(false);
+      return;
+    }
+    clanManager = new ClanManager();
+    hookManager = new HookManager(getServer().getPluginManager());
+    clanChatManager = new ClanChatManager(clanManager, configManager.config());
+    antiFarmManager = new AntiFarmManager();
+  }
+  
+  @Override
+  public void onEnable() {
+    // Checks if the 'hook()' method execution result is a 'NO_HOOKED' enum type.
+    if (hookManager.hook() == HookManager.Result.NO_HOOKED) {
+      LoggerUtils.error("Some dependency could not be hooked by the plugin, check if they are installed and update.");
+    }
+    clanManager.loadClansData();
+  }
+  
+  @Override
+  public void onDisable() {
+    Provider.unload();
+    
+    LoggerUtils.info(
+      "Disabling plugin internal components...",
+      "Clearing cache memory used by the plugin..."
+    );
+    // Checks if the ClanManagerModel reference has not been deleted.
+    if (clanManager != null) {
+      clanManager.clear();
+    }
+  }
+  
   @Override
   public @NotNull ConfigManager configManager() {
     return configManager;
@@ -38,48 +78,9 @@ public final class ClansPlugin extends JavaPlugin implements Clans {
   public @NotNull HookManager hookManager() {
     return hookManager;
   }
-
-  @Override
-  public @NotNull AntiFarmManagerModel antiFarmManagerModel() {
-    return antiFarmManagerModel;
-  }
-
-  @Override
-  public void onLoad() {
-    Provider.load(this);
-    
-    LoggerUtils.info("Loading configuration components for the plugin...");
-    configManager = new ConfigManager(getDataFolder().toPath());
-    // Checks if some configuration model were not loaded correctly.
-    if (configManager.load() == Result.NO_CONFIG_LOAD) {
-      LoggerUtils.error("Some configuration files could not be loaded correctly.");
-      setEnabled(false);
-      return;
-    }
-    clanManager = new ClanManager();
-    hookManager = new HookManager(getServer().getPluginManager());
-    clanChatManager = new ClanChatManager();
-  }
   
   @Override
-  public void onEnable() {
-    // Checks if the 'hook()' method execution result is a 'NO_HOOKED' enum type.
-    if (hookManager.hook() == Result.NO_HOOKED) {
-      LoggerUtils.error("Some dependency could not be hooked by the plugin, check if they are installed and update.");
-    }
-  }
-  
-  @Override
-  public void onDisable() {
-    Provider.unload();
-    
-    LoggerUtils.info(
-      "Disabling plugin internal components...",
-      "Clearing cache memory used by the plugin..."
-    );
-    // Checks if the ClanManagerModel reference has not been deleted.
-    if (clanManager != null) {
-      clanManager.clear();
-    }
+  public @NotNull AntiFarmManager antiFarmManager() {
+    return antiFarmManager;
   }
 }
