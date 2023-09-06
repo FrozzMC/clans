51bef5dd9efecec968d48fcc9a8a3979e64d5bfb
InitSync <benjaalejandro206@gmail.com>
2023-09-05 22:40:07 -0300

    chore(Clans): use new anti-farm manager reference for the method

--- api/src/main/java/net/velex/clans/api/Clans.java
+++ api/src/main/java/net/velex/clans/api/Clans.java
@@ -1,10 +1,10 @@
 package net.velex.clans.api;
 
+import net.velex.clans.api.antifarm.AntiFarmManager;
 import net.velex.clans.api.clan.ClanChatManager;
 import net.velex.clans.api.clan.ClanManager;
 import net.velex.clans.api.config.ConfigManager;
 import net.velex.clans.api.hook.HookManager;
-import net.velex.clans.api.model.*;
 import org.jetbrains.annotations.NotNull;
 
 public interface Clans {
@@ -16,5 +16,5 @@ public interface Clans {
   
   @NotNull HookManager hookManager();
 
-  @NotNull AntiFarmManagerModel antiFarmManagerModel();
+  @NotNull AntiFarmManager antiFarmManager();
 }
